24c2ae36240f35eff028d4ec22388f3615cc3b17
MrCheba <68125070+MrCheba@users.noreply.github.com>
2024-06-04 17:36:30 +0300

    Python_2.py

--- Python_2.py
+++ Python_2.py
@@ -1,40 +1,44 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 import os
 
-A = 0
+# Убедимся, что файл существует
+if not os.path.exists(sys.argv[1]):
+    print(f"Файл {sys.argv[1]} не найден")
+    sys.exit(1)
 
-def f(x):
-    return(0.5 + (((np.sin(x**2 - A**2))**2 - 0.5) / (np.abs(1 + 0.001 * (x**2 + A**2)))))
+# Читаем данные из JSON файла
+with open(sys.argv[1], 'r') as file:
+    data = json.load(file)
 
-start_x = -5
-end_x = 5
-step = 0.2
+x_values = []
+y_values = []
 
-x_values = np.arange(start_x, end_x, step)
-y_values = [f(x) for x in x_values]
-
-
+# Извлекаем данные из JSON
+for x, y in zip(data['x'], data['y']):
+    x_values.append(x)
+    y_values.append(y)
 
 # Строим график
+graf_from_json = plt.subplot()
 plt.plot(x_values, y_values)
 plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.title('f(x)')
+plt.ylabel('y')
+graf_from_json.set_xlim((-5, 5))
+plt.title('Graph of y = f(x)')
 
 def set_yticks_step():
-    if(len(sys.argv) > 2):
-        if ((sys.argv[2]) == "Y"):
+    if len(sys.argv) > 2:
+        if sys.argv[2] == "X":
             step = float(sys.argv[3])
     else:
         step = 1
-    plt.yticks(np.arange(min(y_values), max(y_values)+1, step))
+    plt.xticks(np.arange(-10, 11, step))
 
-
-plt.grid(True)
+set_yticks_step()
 plt.show()
-
 
 
 
